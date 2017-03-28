af51947b8678c0942b69e3748102b58038f3b8b1
johnsonciao <johnsonhsiao0916@gmail.com>
2017-03-28 00:38:50 -0400

    Update main.py

--- BookStore_Web/main.py
+++ BookStore_Web/main.py
@@ -35,7 +35,7 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=80)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 
 @login_manager.user_loader
